2572eab3b5dba419ac11c0910aa595d3cc00ef86
Kerbs <npdannug16@gmail.com>
2021-03-30 16:47:28 +0800

    dev tool initial commit

---
+++ lib/dannugjr_view_tool.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "dannugjr_view_tool/version"
+require_relative "dannugjr_view_tool/renderer"
+
+module DannugjrViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
